4074818099eb3fcb1b62e68662a310ab3ed53845
Mijail Gonzalez <mgonzalez@geotech.com.co>
2021-11-23 14:50:57 -0500

    modificando enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   urlApi: 'https://ultracem.ngrok.io/api-fintra/api/',
   // urlApi: 'http://192.168.190.237:8010/api-fintra/api/',
-  urlApi2: 'https://ultracem.ngrok.io/api-fintra/api/archivos/guardar/',
+  // urlApi2: 'https://ultracem.ngrok.io/api-fintra/api/archivos/guardar/',
   userName: "APPWEB",
   password: "123456",
 };
