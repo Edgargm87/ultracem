33d2a5cad60e8145a8cf59aceff1b82b6cddb96f
Mijail Gonzalez <mgonzalez@geotech.com.co>
2021-12-16 16:01:00 -0500

    validacion pre de persona

---
+++ src/app/shared/modal-acepto-terminos/modal-acepto-terminos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-acepto-terminos',
+  templateUrl: './modal-acepto-terminos.component.html',
+  styleUrls: ['./modal-acepto-terminos.component.scss']
+})
+export class ModalAceptoTerminosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
