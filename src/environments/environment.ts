0d1f861469d17463484bbf4339f07b8885c619dd
SHIRLEY  MUELLE CAMARGO <62040229+SJmuelle@users.noreply.github.com>
2022-01-05 14:33:21 -0500

    bugs de guardado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,9 @@
 export const environment = {
   production: false,
   // urlApi: 'http://192.168.190.237:8010/api-fintra/api/',
-  urlApi: 'http://192.168.140.197:8010/api-fintra/api/',
+  // urlApi: 'http://192.168.140.197:8010/api-fintra/api/',
+  urlApi: 'https://ultracem.ngrok.io/api-fintra/api/',
+
   // urlApi2: 'https://ultracem.ngrok.io/api-fintra/api/archivos/guardar/',
   // urlApi: 'https://ultracem.ngrok.io/api-fintra/api/',
   userName: "APPWEB",
