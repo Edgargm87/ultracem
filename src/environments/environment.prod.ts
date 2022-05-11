e3c478561e85959cf6dd16b162867e2761f322ca
SHIRLEY  MUELLE CAMARGO <62040229+SJmuelle@users.noreply.github.com>
2022-05-11 11:02:44 -0500

    cambios de la url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  urlApi: 'https://prometheus.fintra.co:8443/api-fintra/api/',
+  urlApi: 'https://ultracem.ngrok.io/api-fintra/api/',
   // urlApi: 'http://192.168.190.237:8010/api-fintra/api/',
   // urlApi2: 'https://ultracem.ngrok.io/api-fintra/api/archivos/guardar/',
   userName: "APPWEB",
   password: btoa("123456"),
   reconocer:"https://recidaw.olimpiait.com",
   // apiCredit: "https://prometheus.fintra.co:8443/fintracredit",
-  apiCredit: "http://prometheus.fintra.co:8084/fintracredit",
+  apiCredit: 'https://ultracem.ngrok.io/api-fintra/api/',
 };
