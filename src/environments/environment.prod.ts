4e025f5ddd434d1457068e7cc119e7a327609de9
tavosalas27 <102743153+tavosalas27@users.noreply.github.com>
2022-11-16 15:23:06 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  urlApi: 'https://2c01-181-57-229-82.ngrok.io/api-fintra/api/',
+  urlApi: 'https://6f10-181-57-229-82.ngrok.io/api-fintra/api/',
   // urlApi: 'http://192.168.190.237:8010/api-fintra/api/',
   // urlApi2: 'https://ultracem.ngrok.io/api-fintra/api/archivos/guardar/',
   userName: "APPWEB",
   password: btoa("123456"),
   reconocer:"https://recidaw.olimpiait.com",
   // apiCredit: "https://prometheus.fintra.co:8443/fintracredit",
-  apiCredit: 'https://2c01-181-57-229-82.ngrok.io/api-fintra/api/',
+  apiCredit: 'https://6f10-181-57-229-82.ngrok.io/api-fintra/api/',
 };
